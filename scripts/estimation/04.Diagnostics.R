f32b0317749951a8f64e42393c7fba7026484a9f
smjenness <smjenness@gmail.com>
2018-09-10 11:04:10 -0400

    Update diagnostics

--- scripts/estimation/04.Diagnostics.R
+++ scripts/estimation/04.Diagnostics.R
@@ -21,7 +21,6 @@ est <- readRDS(file = fn)
 # Main --------------------------------------------------------------------
 
 fit_main <- est[[1]]
-# mcmc.diagnostics(fit_main$fit, vars.per.page = 8)
 
 model_main_dx <- ~edges +
   nodematch("age.grp", diff = TRUE) +
@@ -37,14 +36,12 @@ dx_main <- netdx(fit_main, nsims = 20, ncores = 12, nsteps = 500,
             nwstats.formula = model_main_dx,
             set.control.ergm = control.simulate.ergm(MCMC.burnin = 1e5))
 print(dx_main)
-rm(dx_main); gc()
 # plot(dx_main)
 
 
 # Casual ------------------------------------------------------------------
 
 fit_casl <- est[[2]]
-# mcmc.diagnostics(fit_main$fit, vars.per.page = 8)
 
 model_casl_dx <- ~edges +
   nodematch("age.grp", diff = TRUE) +
@@ -60,7 +57,6 @@ dx_casl <- netdx(fit_casl, nsims = 20, ncores = 12, nsteps = 500,
             nwstats.formula = model_casl_dx,
             set.control.ergm = control.simulate.ergm(MCMC.burnin = 1e5))
 print(dx_casl)
-rm(dx_casl); gc()
 # plot(dx_casl)
 
 dx_casl <- netdx(fit_casl, nsims = 5000, dynamic = FALSE,
@@ -71,7 +67,6 @@ print(dx_casl)
 # One-Off -----------------------------------------------------------------
 
 fit_inst <- est[[3]]
-# mcmc.diagnostics(fit_inst$fit, vars.per.page = 8)
 
 model_inst_dx <- ~edges +
   nodematch("age.grp", diff = FALSE) +
