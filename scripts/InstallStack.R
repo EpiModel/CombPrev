ec0b8b77dc5123efbb03d1642338bd26554880ec
smjenness <smjenness@gmail.com>
2019-04-05 10:26:47 -0400

    Update InstallStack.R

--- scripts/InstallStack.R
+++ scripts/InstallStack.R
@@ -1,21 +1,26 @@
 
-# Install EpiModel Stack
+
+# Core Package Stack for Project ------------------------------------------
+
+# Base EpiModel
 install.packages("EpiModel")
 
-# Install Extra Helper Packages
+# Extra Helper Packages
 install.packages(c("remotes", "sessioninfo"))
 
-# Latest Dev Versions of Packages
+# Latest Dev Versions of Statnet Packages
 remotes::install_github(c("statnet/network",
                           "statnet/statnet.common",
                           "statnet/ergm",
                           "statnet/tergm"))
 
+# Latest Dev Versions of EpiModel Packages
 remotes::install_github(c("statnet/EpiModel",
                           "statnet/EpiModelHPC",
                           "statnet/tergmLite",
                           "EpiModel/EpiABC"))
 
+# Current Version of EpiModelHIV for Project
 remotes::install_github("EpiModel/EpiModelHIV-p", ref = "CombPrev")
 
 
@@ -23,4 +28,3 @@ remotes::install_github("EpiModel/EpiModelHIV-p", ref = "CombPrev")
 
 library("EpiModelHIV")
 sessioninfo::session_info()
-
