0c214f5858b40e5b664b20ef8bd97a7f02d00b9e
smjenness <smjenness@gmail.com>
2019-06-17 13:09:35 -0400

    Update base burnin files

--- burnin/master.R
+++ burnin/master.R
@@ -5,11 +5,10 @@ library("EpiModelHPC")
 
 # Calibration/Testing -----------------------------------------------------
 
+# Test stability of run
 vars <- NULL
 sbatch_master(vars = vars,
               master.file = "burnin/master.sh",
-              runsim.file = "runsim.sh",
-              param.sheet = "burnin/params.csv",
               simno.start = 100,
               ckpt = TRUE,
               nsims = 250,
@@ -17,12 +16,10 @@ sbatch_master(vars = vars,
               walltime = "00:30:00",
               mem = "100G")
 
-vars <- NULL
+# Big run batch for model selection
 sbatch_master(vars = vars,
               master.file = "burnin/master.sh",
-              runsim.file = "runsim.sh",
-              param.sheet = "burnin/params.csv",
-              simno.start = 1001,
+              simno.start = 200,
               ckpt = TRUE,
               nsims = 25000,
               ncores = 28,
