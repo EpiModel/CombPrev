cf83d3f8c57c5a5c60ef90100ff3311023988e7e
smjenness <smjenness@gmail.com>
2019-06-17 13:09:47 -0400

    Clean up sim.select

--- burnin/post/sim.select.R
+++ burnin/post/sim.select.R
@@ -1,13 +1,17 @@
 
-## File processing -------------------------------------------------------------
+##
+## Selects the simulations with the smallest deviance from targets for intervention
+##
 
 suppressMessages(library("EpiModelHIV"))
 suppressMessages(library("tidyverse"))
 suppressMessages(library("foreach"))
 
+# List Files
 list.files("data/")
 fn <- list.files("data/", pattern = "sim", full.names = TRUE)
 
+# Create outcome data frame for comparison
 doParallel::registerDoParallel(32)
 tdf <- foreach(i = 1:length(fn)) %dopar% {
   load(fn[i])
@@ -23,26 +27,21 @@ tdf <- foreach(i = 1:length(fn)) %dopar% {
 }
 
 tdf <- data.frame(do.call("rbind", tdf), stringsAsFactors = FALSE)
-head(tdf, 100); str(tdf)
 names(tdf)[1] <- "batch"
 tdf[2:4] <- sapply(tdf[2:4], as.numeric)
-save(tdf, file = "data/tdf.rda")
+head(tdf, 25); str(tdf)
 
-load("data/tdf.rda")
-tdf_sel <- tdf[which(tdf$i.prev.dx.B > 0.332 & tdf$i.prev.dx.B < 0.334 &    ## 0.333
-                     tdf$i.prev.dx.H > 0.126 & tdf$i.prev.dx.H < 0.128 &    ## 0.127
-                     tdf$i.prev.dx.W > 0.083 & tdf$i.prev.dx.W < 0.085), ]  ## 0.084
-tdf_sel
+save(tdf, file = "data/hold/tdf.rda")
 
-tdf_sel <- tdf[which(round(tdf$i.prev.dx.B, 2) == 0.33 &
-                     round(tdf$i.prev.dx.H, 3) == 0.127 &
-                     round(tdf$i.prev.dx.W, 3) == 0.084), ]
-tdf_sel
+# Simulation selection
+targets <- c(0.333, 0.127, 0.084)
+tdf$diff <- abs(tdf$i.prev.dx.B - targets[1]) +
+            abs(tdf$i.prev.dx.H - targets[2]) +
+            abs(tdf$i.prev.dx.W - targets[3])
+head(arrange(tdf, diff), 25)
 
-#       batch         i.prev.dx.B i.prev.dx.H i.prev.dx.W
-# 10726 n1001.442.2   0.3327749   0.1267496  0.08396303
-
-load("data/sim.n1001.787.rda")
+# Verify outcomes
+load("data/sim.nXXX.rda")
 ls()
 s1 <- get_sims(sim, sims = 25)
 
@@ -52,9 +51,11 @@ df <- tail(df, 52)
 df
 colMeans(df)
 
-# Save as best-fitting
+# Save selected simulation
 sim <- s1
-
 saveRDS(sim, file = "est/burnin.ATL.3race.rda", compress = "xz")
+
+# Hold the full netsim file
 system("mv data/sim.n1001.787.rda data/hold/")
-system("mv data/tdf.rda data/hold/")
+
+# Reinstall EpiModelHIV before running interventions
