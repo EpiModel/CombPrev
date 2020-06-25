5a1a496b032b7e5018beababf45ccd4fe7faec34
Samuel Jenness <smjenness@gmail.com>
2020-06-25 12:24:32 -0400

    Update InstallStack.R
    Fix EpiModelHPC version

--- InstallStack.R
+++ InstallStack.R
@@ -18,7 +18,7 @@ remotes::install_github(c("statnet/network@deff2a0",
 
 # Latest Dev Versions of EpiModel Packages
 remotes::install_github(c("statnet/EpiModel@2c131f0",
-                          "statnet/EpiModelHPC@a64dbf2",
+                          "statnet/EpiModelHPC@v2.1.0",
                           "statnet/tergmLite@v1.2.0",
                           "EpiModel/EpiABC@c32ecb6",
                           "EpiModel/ARTnetData@1d8ec6e",
@@ -44,9 +44,9 @@ sessioninfo::package_info(pkgs = c("network", "networkDynamic", "statnet.common"
 # ARTnet           1.0.0       2020-06-19 [1] Github (EpiModel/ARTnet@150c631)
 # ARTnetData       1.0         2020-06-19 [1] Github (EpiModel/ARTnetData@1d8ec6e)
 # EpiABC           1.0         2019-05-16 [1] Github (EpiModel/EpiABC@c32ecb6)
-# EpiModel       * 1.7.3       2020-06-24 [1] Github (statnet/EpiModel@2c131f0)
+# EpiModel       * 1.7.3       2020-06-25 [1] Github (statnet/EpiModel@2c131f0)
 # EpiModelHIV    * 1.5.0       2020-06-25 [1] Github (EpiModel/EpiModelHIV-p@d72a6eb)
-# EpiModelHPC    * 2.0.2       2020-06-19 [1] Github (statnet/EpiModelHPC@a64dbf2)
+# EpiModelHPC    * 2.1.0       2020-06-25 [1] Github (statnet/EpiModelHPC@257bbf3)
 # ergm           * 3.10.0-4851 2020-06-24 [1] Github (statnet/ergm@8b30e92)
 # network        * 1.14-377    2020-06-24 [1] Github (statnet/network@deff2a0)
 # networkDynamic * 0.10        2020-06-24 [1] Github (statnet/networkDynamic@14182bf)
